3ac868ea10db5eb62e02afa144cc04734494e0c4
Matheus Richard <matheusrichardt@gmail.com>
2018-11-16 16:38:23 -0200

    Adding tests to Player

--- test/player_test.rb
+++ test/player_test.rb
@@ -3,33 +3,49 @@ require_relative "test_helper"
 class TestHelper < Minitest::Test
   def setup
     @player = Player.new("Awesome Name", 16, 8)
-
   end
   attr_accessor :player
 
   def test_should_create_player
-
     assert_equal "Awesome Name", player.name
     assert_equal 16, player.energy
     assert_equal 8, player.ability
   end
 
-  def test_should_create_player_with_random_attributes
-    my_player = Player.new("Passing just the name")
+  def test_should_not_create_player_without_name
+    assert_raises ArgumentError do
+      Player.new
+    end
+  end
 
-    assert_equal "Passing just the name", my_player.name
-    refute my_player.energy.nil?
-    refute my_player.ability.nil?
+  def test_should_create_player_with_random_attributes_if_they_were_not_provided
+    dice_value = 5
+    expected_energy = 12 + dice_value
+    expected_ability = 6 + dice_value
+    Player.any_instance.expects(:roll_dice).twice.returns(dice_value)
+    
+    name = "Just-the-name"
+    my_player = Player.new(name)
+
+    assert_equal name, my_player.name
+    assert_equal expected_energy, my_player.energy
+    assert_equal expected_ability, my_player.ability
   end
 
   def test_player_is_alive
-    refute @player.dead?
+    refute player.dead?
   end
 
   def test_player_is_dead
-    @player.energy = 0
+    player.energy = 0
     assert player.dead?
-    @player.energy = -2
+    player.energy = -2
     assert player.dead?
+  end
+
+  def test_player_attack
+    player.expects(:roll_dice).twice.returns(5)
+
+    assert_equal (5 + 5 + player.ability), player.attack
   end
 end
