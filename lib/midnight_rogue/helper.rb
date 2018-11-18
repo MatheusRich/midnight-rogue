224564a2eb48f9f3904afc5ac205bc7652c1c128
Matheus Richard <matheusrichardt@gmail.com>
2018-11-18 15:29:36 -0200

    Getting user input

--- lib/midnight_rogue/helper.rb
+++ lib/midnight_rogue/helper.rb
@@ -15,4 +15,24 @@ module Helper
       sleep 0.25
     end
   end
+
+
+
+  def get_user_option(valid_options)
+    loop do
+      puts "[ ? ] What are you going to do?"
+      input = STDIN.gets.chomp
+
+      return input if valid_options.include? input
+      exit_game if input.downcase == "exit"
+
+      puts "'#{input}' is not a valid option.\n\nTry again:"
+    end
+  end
+
+  def exit_game
+    clear_screen
+    exit
+  end
+
 end
--- test/helper_test.rb
+++ test/helper_test.rb
@@ -25,4 +25,57 @@ class HelperTest < Minitest::Test
     assert_equal ". . . . .", output
     assert_empty errors
   end
+
+  def test_get_user_option
+    valid_options = ["1", "2", "3"]
+    user_input = "1"
+    STDIN.expects(:gets).returns(user_input)
+
+    capture_io do
+      selected_option = get_user_option(valid_options)
+      assert_equal user_input, selected_option
+    end
+  end
+
+  def test_user_selects_exit_option
+    valid_options = ["1"]
+    possible_user_inputs = ["exit", "EXIT", "ExIt"]
+
+    possible_user_inputs.each do |user_input|
+      STDIN.expects(:gets).returns(user_input)
+      expects(:clear_screen)
+
+      capture_io do
+        assert_raises SystemExit do
+          selected_option = get_user_option(valid_options)
+        end
+      end
+    end
+  end
+
+  def test_user_inserts_an_invalid_option_and_after_a_valid_one
+    valid_options = ["1", "2"]
+    first_user_input = "0"
+    second_user_input = "1"
+    invalid_input_message = <<~HEREDOC
+      '0' is not a valid option.
+
+      Try again:
+    HEREDOC
+    STDIN.expects(:gets).twice.returns(first_user_input, second_user_input)
+
+    selected_option = ""
+    output, errors = capture_io do
+      selected_option = get_user_option(valid_options)
+    end
+    assert_match invalid_input_message, output
+    assert_equal second_user_input, selected_option
+    assert_empty errors
+  end
+
+  def test_exit_game
+    expects(:clear_screen)
+    expects(:exit)
+    exit_game
+  end
 end
