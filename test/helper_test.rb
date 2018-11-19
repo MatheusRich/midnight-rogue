cc7e4651e05ad2c341c65dc352152a4046cfc7b6
Matheus Richard <matheusrichardt@gmail.com>
2018-11-19 00:14:32 -0200

    Adding Helper tests

--- test/helper_test.rb
+++ test/helper_test.rb
@@ -26,56 +26,62 @@ class HelperTest < Minitest::Test
     assert_empty errors
   end
 
-  def test_get_user_option
-    valid_options = ["1", "2", "3"]
+  def test_get_user_choice
+    valid_choices = [1, 2, 3]
     user_input = "1"
-    STDIN.expects(:gets).returns(user_input)
+    STDIN.stubs(:gets).returns(user_input)
 
     capture_io do
-      selected_option = get_user_option(valid_options)
-      assert_equal user_input, selected_option
+      selected_choice = get_user_choice(valid_choices)
+      assert_equal user_input, selected_choice
     end
   end
 
-  def test_user_selects_exit_option
-    valid_options = ["1"]
+  def test_user_selects_exit_choice
+    valid_choices = [1]
     possible_user_inputs = ["exit", "EXIT", "ExIt"]
 
     possible_user_inputs.each do |user_input|
       STDIN.expects(:gets).returns(user_input)
       expects(:clear_screen)
 
-      capture_io do
+      output, errors = capture_io do
         assert_raises SystemExit do
-          selected_option = get_user_option(valid_options)
+          selected_choice = get_user_choice(valid_choices)
         end
       end
+
+      assert_match /Cya!/, output
+      assert_empty errors
     end
   end
 
-  def test_user_inserts_an_invalid_option_and_after_a_valid_one
-    valid_options = ["1", "2"]
+  def test_user_inserts_an_invalid_choice_and_after_a_valid_one
+    valid_choices = [1, 2]
     first_user_input = "0"
     second_user_input = "1"
     invalid_input_message = <<~HEREDOC
-      '0' is not a valid option.
+      '0' is not a valid choice.
 
       Try again:
     HEREDOC
     STDIN.expects(:gets).twice.returns(first_user_input, second_user_input)
 
-    selected_option = ""
+    selected_choice = ""
     output, errors = capture_io do
-      selected_option = get_user_option(valid_options)
+      selected_choice = get_user_choice(valid_choices)
     end
     assert_match invalid_input_message, output
-    assert_equal second_user_input, selected_option
+    assert_equal second_user_input, selected_choice
     assert_empty errors
   end
 
   def test_exit_game
     expects(:clear_screen)
     expects(:exit)
-    exit_game
+    
+    capture_io do
+      exit_game
+    end
   end
 end
